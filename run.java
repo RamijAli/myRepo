23fe90e6b7627ee174f9546c9b976df3726ec0f5
Ali <ramij.ali@spglobal.com>
2021-06-07 17:38:15 +0530

    First Commit

---
+++ run.java
@@ -0,0 +1,6 @@
+class run{
+	public static void main(String[] args){
+		Test test1=new Test();
+		system.out.println(test1.result());
+	}
+}
---
+++ test.java
@@ -0,0 +1,7 @@
+class Test{
+	public  int serial;
+	public String name;
+	public String result(){
+		return "test done";
+	}
+}
